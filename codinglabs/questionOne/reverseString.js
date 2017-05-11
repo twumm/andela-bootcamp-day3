cb0269e49d9588f9bb388f2d459aab3342f3cfbc
twumm <martint.mensah@gmail.com>
2017-05-11 12:13:34 +0100

    Add coding labs folder to the branch

---
+++ codinglabs/questionOne/reverseString.js
@@ -0,0 +1,24 @@
+/*You are to write a function, `reverseString()`, that returns the reverse of the string argument provided. If the reverse of the string is the same as the original string, as in the case of palindromes, return `true` instead.*/
+
+/*function name - reverseString() takes a word
+loop through the word from the back
+store each letter from the back to a new var till the end of the word
+compare the new var to the original word
+if they are the same, return true*/
+
+function reverseString(word){
+  var reverse = '';
+  for (var i = word.length-1; i >= 0; i--){
+    reverse += word[i];
+  }
+  if (typeof word !== null && typeof value === 'undefined' && reverse === word){
+    return true;
+  }
+  else if (word !== reverse){
+    return reverse;
+  }
+  else {
+    return "null";
+  }
+  //console.log(reverse);
+}
